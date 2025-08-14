b19373a9cfd08a1c9fed9206f9cd8e81330c92ed
maridevdata <marianaluisa.works@gmail.com>
2025-08-14 17:32:58 -0300

    Ajustes Finais

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,26 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
+import 'react-toastify/dist/ReactToastify.css'
 import './App.css'
 import Footer from './components/footer/Footer'
 import Navbar from './components/navbar/Navbar'
+import FormPostagem from './components/postagens/formpostagem/FormPostagem'
+import ListaPostagens from './components/postagens/listapostagens/ListaPostagens'
+import DeletarPostagem from './components/postagens/deletarpostagem/DeletarPostagem'
+import DeletarTema from './components/temas/deletartema/DeletarTema'
+import FormTema from './components/temas/formtema/FormTema'
+import ListaTemas from './components/temas/listatemas/ListaTemas'
+import { AuthProvider } from './contexts/AuthContext'
+import Cadastro from './pages/cadastro/Cadastro'
 import Home from './pages/home/Home'
 import Login from './pages/login/Login'
-import Cadastro from './pages/cadastro/Cadastro'
+import Perfil from './pages/perfil/Perfil'
+import { ToastContainer } from 'react-toastify'
 
 function App() {
   return (
     <>
+      <AuthProvider>
+        <ToastContainer />
         <BrowserRouter>
           <Navbar />
           <div className="min-h-[80vh]">
@@ -17,10 +29,20 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastrartema" element={<FormTema />} />
+              <Route path="/editartema/:id" element={<FormTema />} />
+              <Route path="/deletartema/:id" element={<DeletarTema />} />
+              <Route path="/postagens" element={<ListaPostagens />} />
+              <Route path="/cadastrarpostagem" element={<FormPostagem />} />
+              <Route path="/editarpostagem/:id" element={<FormPostagem />} />
+              <Route path="/deletarpostagem/:id" element={<DeletarPostagem />} />
+              <Route path="/perfil" element={<Perfil />} />
             </Routes>
           </div>
           <Footer />
         </BrowserRouter>
+      </AuthProvider>
     </>
   )
 }
--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -1,31 +1,44 @@
-import { GithubLogoIcon, InstagramLogoIcon, LinkedinLogoIcon } from '@phosphor-icons/react'
+import { FacebookLogo, InstagramLogo, LinkedinLogo } from '@phosphor-icons/react'
+import { type ReactNode, useContext } from 'react'
+import { AuthContext } from '../../contexts/AuthContext'
 
 function Footer() {
 
-    // eslint-disable-next-line prefer-const
     let data = new Date().getFullYear()
 
-    return (
-        <>
+    const { usuario } = useContext(AuthContext)
+
+    let component: ReactNode
+
+    if (usuario.token !== "") {
+
+        component = (
+
             <div className="flex justify-center bg-indigo-900 text-white">
                 <div className="container flex flex-col items-center py-4">
                     <p className='text-xl font-bold'>
-                            Blog Pessoal | Copyright: {data}
-                        </p>
-                    <p className='text-lg'>Acesse minhas redes sociais</p>
+                        Blog Pessoal Generation | Copyright: {data}
+                    </p>
+                    <p className='text-lg'>Acesse nossas redes sociais</p>
                     <div className='flex gap-2'>
-                        <a href="https://www.linkedin.com/in/mariana-luisa/" target="_blank">
-                        <LinkedinLogoIcon size={48} weight='bold' />
+                        <a href="https://www.linkedin.com/school/generationbrasil" target="_blank">
+                            <LinkedinLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://www.instagram.com/marilu.isa_/" target="_blank">
-    	                        <InstagramLogoIcon size={48} weight='bold' />
+                        <a href="https://www.instagram.com/generationbrasil" target="_blank">
+                            <InstagramLogo size={48} weight='bold' />
                         </a>
-                        <a href="https://github.com/maridevdata" target="_blank">
-    	                        <GithubLogoIcon size={48} weight='bold' />
-	                    </a>
+                        <a href="https://www.facebook.com/generationbrasil" target="_blank">
+                            <FacebookLogo size={48} weight='bold' />
+                        </a>
                     </div>
                 </div>
             </div>
+
+        )
+    }
+    return (
+        <>
+            { component }
         </>
     )
 }
